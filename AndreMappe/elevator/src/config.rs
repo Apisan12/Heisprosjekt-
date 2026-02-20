9ecd2a2b70694e8ac27a64d9189ae287f6a0c8ae
OASS <olav.storholm@gmail.com>
2026-02-20 09:33:53 +0100

    Comments

--- AndreMappe/elevator/src/config.rs
+++ AndreMappe/elevator/src/config.rs
@@ -1,8 +1,8 @@
 // This module holds all static program parameters used in one place.
 use std::time::Duration;
 
-// Default numbers of floors in the lab.
-pub const DEFAULT_NUM_FLOORS: usize = 4;
+// Number of floors in the elevator.
+pub const ELEV_NUM_FLOORS: u8 = 4;
 
 // Duration between elevator hardware polls
 pub const ELEV_POLL: Duration = Duration::from_millis(25);
---
+++ AndreMappe/elevator/src/init.rs
@@ -0,0 +1,8 @@
+//TODO
+//Lage INIT funksjoner
+//Flytte fra main
+//Lage oversiktlige funksjon navn
+
+
+//Lage unik ID for alle calls
+//Fikse sletting av ordre*
