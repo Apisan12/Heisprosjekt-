b4e153a71bc752336e7b8eda227bc0219204c6c2
OASS <olav.storholm@gmail.com>
2026-02-20 09:19:11 +0100

    La til default const

--- AndreMappe/elevator/src/config.rs
+++ AndreMappe/elevator/src/config.rs
@@ -1,6 +1,12 @@
+// This module holds all static program parameters used in one place.
 use std::time::Duration;
 
+// Default numbers of floors in the lab.
+pub const DEFAULT_NUM_FLOORS: usize = 4;
 
-
-/// Duration between elevator hardware polls
+// Duration between elevator hardware polls
 pub const ELEV_POLL: Duration = Duration::from_millis(25);
+
+
+
+
