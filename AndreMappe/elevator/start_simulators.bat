7ec361c04e2b8ce9a50923b8f27d0f6949f9cb1c
OASS <olav.storholm@gmail.com>
2026-03-20 11:03:10 +0100

    Final

--- AndreMappe/elevator/start_simulators.bat
+++
@@ -1,3 +0,0 @@
-start cmd /k "mode con: cols=60 lines=25 & SimElevatorServer.exe --port 15657"
-start cmd /k "mode con: cols=60 lines=25 & SimElevatorServer.exe --port 15658"
-start cmd /k "mode con: cols=60 lines=25 & SimElevatorServer.exe --port 15659"
